40469794464623f2b952428e59c41933ee1c9e32
Samuel Roberts <sproberts92@outlook.com>
2017-04-09 17:57:38 +0200

    Add time setup
    Set NTP and timezone.

--- arch-install.sh
+++ arch-install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Exit if any individual command fails.
+set -e
+
 # Verify boot mode
 if [[ ! -d /sys/firmware/efi/efivars ]]; then
 	echo "Cannot verify UEFI boot mode."
 	exit 1
 fi
+
+# Set network time sync and timezone.
+timedatectl set-ntp true
+timedatectl set-timezone Europe/Amsterdam
 
