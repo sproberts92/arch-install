ae5a0f4dadb6a2a6a64a0b3aff43269e49054553
Samuel Roberts <sproberts92@outlook.com>
2017-04-15 20:52:30 +0200

    Add partition mounting
    ESP partition is mounted in /mnt/boot/.

--- partition-fresh.sh
+++ partition-fresh.sh
@@ -6,3 +6,11 @@ parted --script /dev/sda \
 	mkpart ESP fat32 1MiB 513MiB \
 	set 1 boot on \
 	mkpart primary ext4 513MiB 100%
+
+mkfs.vfat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+
+mount /dev/sda2 /mnt
+
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
