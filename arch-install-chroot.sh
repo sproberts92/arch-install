d851d7692da2c90287b7f961436f194b1c8246db
Samuel Roberts <sproberts92@outlook.com>
2017-04-16 02:48:06 +0200

    Add boot configuration
    At the moment includes specific VirtualBox config, this will later
    be set as an option.

--- arch-install-chroot.sh
+++ arch-install-chroot.sh
@@ -13,3 +13,11 @@ echo "LANG=${language}" > "/etc/locale.conf"
 
 echo "${hostn}" > "/etc/hostname"
 sed -i "/::1/a 127.0.1.1\t${hostn}.localdomain\t${hostn}" hosts
+
+grub-install --target=x86_64-efi --efi-directory=boot --bootloader-id=arch_grub
+grub-mkconfig -o "/boot/grub/grub.cfg"
+
+mkinitcpio -p linux
+
+mkdir "/boot/EFI/boot"
+cp "/boot/EFI/arch_grub/grubx64.efi" "/boot/EFI/boot/bootx64.efi"
