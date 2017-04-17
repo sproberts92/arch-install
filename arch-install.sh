91d9f68a7796f7d9a29434ba41fed24e12bea175
Samuel Roberts <sproberts92@outlook.com>
2017-04-17 15:29:05 +0200

    Combine arch-install-chroot.sh into arch-install.sh
    Call each command in a separate chroot environment, so that variables
    from outside can more easily be used inside.

--- arch-install.sh
+++ arch-install.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+# Exit if any individual command fails.
+set -e
+source $1
+
 pacstrap /mnt base grub efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
 
-cat arch-install-chroot.sh | arch-chroot /mnt
+for loc in $locales
+do
+	arch-chroot /mnt sed -i "/${loc}/s/^#//" "/etc/locale.gen"
+done
+
+arch-chroot /mnt locale-gen
+
+arch-chroot /mnt echo "LANG=${language}" > "/etc/locale.conf"
+
+arch-chroot /mnt echo "${host_name}" > "/etc/hostname"
+arch-chroot /mnt sed -i "/::1/a 127.0.1.1\t${host_name}.localdomain\t${host_name}" hosts
+
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=boot --bootloader-id=arch_grub
+arch-chroot /mnt grub-mkconfig -o "/boot/grub/grub.cfg"
+
+arch-chroot /mnt mkinitcpio -p linux
+
+arch-chroot /mnt mkdir "/boot/EFI/boot"
+arch-chroot /mnt cp "/boot/EFI/arch_grub/grubx64.efi" "/boot/EFI/boot/bootx64.efi"
