f1c38a3250897289c4e2897ff2190453fc61e7fe
Samuel Roberts <sproberts92@outlook.com>
2017-04-17 23:48:47 +0200

    Fix variable substitution
    Need to break out variable substituion from literal strings.

--- arch-install.sh
+++ arch-install.sh
@@ -8,20 +8,20 @@ pacstrap /mnt base grub efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
 
 a_chroot() {
-	arch-chroot /mnt /bin/bash -c $1
+	arch-chroot /mnt /bin/bash -c "${1}"
 }
 
 for loc in $locales
 do
-	a_chroot 'sed -i "/${loc}/s/^#//" "/etc/locale.gen"'
+	a_chroot 'sed -i "/'"${loc}"'/s/^#//" "/etc/locale.gen"'
 done
 
 a_chroot 'locale-gen'
 
-a_chroot 'echo "LANG=${language}" > "/etc/locale.conf"'
+a_chroot 'echo "LANG='"${language}"'" > "/etc/locale.conf"'
 
-a_chroot 'echo "${host_name}" > "/etc/hostname"'
-a_chroot 'sed -i "/::1/a 127.0.1.1\t${host_name}.localdomain\t${host_name}" "/etc/hosts"'
+a_chroot 'echo "'"${host_name}"'" > "/etc/hostname"'
+a_chroot 'sed -i "/::1/a 127.0.1.1\t'"${host_name}"'.localdomain\t'"${host_name}"'" "/etc/hosts"'
 
 a_chroot 'grub-install --target=x86_64-efi --efi-directory=boot --bootloader-id=arch_grub'
 a_chroot 'grub-mkconfig -o "/boot/grub/grub.cfg"'
