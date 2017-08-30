8ae388a6685911e21142d8f2888f6de244d0ab59
Samuel Roberts <sproberts92@outlook.com>
2017-08-31 00:16:34 +0200

    Correct call of post script

--- arch-install.sh
+++ arch-install.sh
@@ -45,4 +45,4 @@ EOF'
 cp "$(dirname $(realpath $0))/arch-install-post.sh" '/mnt/root'
 cp "$(realpath $1)" '/mnt/root'
 
-a_chroot '~/arch-install-post.sh ~/arch-install-post.cfg'
+a_chroot "~/arch-install-post.sh ~/$(basename $1)"
